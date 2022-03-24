d4b41028995b3012d21f9c7723d05e0b9a5dab7d
Christopher Gray <gray.chris@protonmail.com>
2022-03-24 18:40:11 -0400

    Adds basic multithreading

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ COP4520 Team 10 Research Project
 
 Implementation of https://raytracing.github.io/books/RayTracingInOneWeekend.html
 
-Compile & Run: g++ ./src/main.cc -o program && ./program > Image.ppm
+Compile & Run: g++ ./src/main.cc -pthread -o program && ./program > Image.ppm
--- src/main.cc
+++ src/main.cc
@@ -1,12 +1,18 @@
 #include "rtweekend.h"
-
+	
 #include "camera.h"
 #include "color.h"
 #include "hittable_list.h"
 #include "material.h"
 #include "sphere.h"
 
+#include <chrono>
+#include <functional>
 #include <iostream>
+#include <sstream>
+#include <thread>
+
+const int num_threads = std::thread::hardware_concurrency();
 
 color ray_color(const ray& r, const hittable& world, int depth) {
 	hit_record rec;
@@ -74,6 +80,26 @@ hittable_list random_scene() {
 	return world;
 }
 
+void render(int start, int end, std::stringstream & out, int image_width, int image_height,
+			int samples_per_pixel, int max_depth, camera & cam, hittable_list & world) {
+
+
+	for (int j = start; j >= end; --j) {
+		for (int i = 0; i < image_width; ++i) {
+			color pixel_color(0, 0, 0);
+			for (int s = 0; s < samples_per_pixel; ++s) {
+				auto u = (i + random_double()) / (image_width-1);
+				auto v = (j + random_double()) / (image_height-1);
+				ray r = cam.get_ray(u, v);
+				pixel_color += ray_color(r, world, max_depth);
+			}
+		write_color(out, pixel_color, samples_per_pixel);
+		}	
+	}
+
+	std::cerr << "Block done.\n";
+}
+
 int main() {
 	
 	// Image
@@ -81,8 +107,8 @@ int main() {
 	const auto aspect_ratio = 3.0 / 2.0;
 	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 500;
-	const int max_depth = 50;
+	const int samples_per_pixel = 100;
+	const int max_depth = 10;
 
 	// World
 
@@ -101,23 +127,38 @@ int main() {
 
 	// Render
 
+	std::cerr << "Rendering with " << num_threads << " threads.\n";
+
+	auto start = std::chrono::high_resolution_clock::now();
+	std::thread threads[num_threads];
+	std::stringstream streams[num_threads];
+	int block_size = image_height/num_threads;
+
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	for (int j = image_height-1; j >= 0; --j) {
-		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-		for (int i = 0; i < image_width; ++i) {
-			color pixel_color(0, 0, 0);
-			for (int s = 0; s < samples_per_pixel; ++s) {
-				auto u = (i + random_double()) / (image_width-1);
-				auto v = (j + random_double()) / (image_height-1);
-				ray r = cam.get_ray(u, v);
-				pixel_color += ray_color(r, world, max_depth);
-			}
-			write_color(std::cout, pixel_color, samples_per_pixel);
-		}	
+	int s = image_height - 1;
+	for (int i = 0; i < num_threads - 1; ++i, s -= block_size + 1) {
+		streams[i] = std::stringstream();
+		threads[i] = std::thread(render, s, s - block_size, std::ref(streams[i]), image_width, image_height,
+								 samples_per_pixel, max_depth, std::ref(cam), std::ref(world));
 	}
 
+	threads[num_threads - 1] = std::thread(render, s, 0, std::ref(streams[num_threads - 1]), image_width,
+										   image_height, samples_per_pixel, max_depth, std::ref(cam), std::ref(world));
+
+	for (std::thread & t : threads)
+		t.join();
+
+	for (int i = 0; i < num_threads; ++i)
+		std::cout << streams[i].str();
+
+
+	auto end = std::chrono::high_resolution_clock::now();
+
 	std::cerr << "\nDone.\n";
+	std::cerr << "Took " 
+			  << std::chrono::duration_cast<std::chrono::seconds>(end - start).count() 
+			  << " seconds to render.\n";
 
 	return 0;
 }
