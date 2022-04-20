6b8fbbf286bca41fbdc96f9d67f16cb798345b43
AshenSilverwolf <gilbert.gavin@outlook.com>
2022-04-20 03:05:48 -0400

    progress made on scene3() 'Fibonacci Spiral'

--- src/main.cc
+++ src/main.cc
@@ -102,11 +102,11 @@ hittable_list scene1() {
 	auto ground_material = make_shared<lambertian>(color(0.1, 0.1, 0.1));
 	world.add(make_shared<sphere>(point3(0,-100,0), 100, ground_material));
 
-	auto material_red = make_shared<metal>(color(1.0, 0.0, 0.0), 0.8);
+	auto material_red = make_shared<metal>(color(1.0, 0.0, 0.0), 0.0);
 	world.add(make_shared<sphere>(point3(0,1,1), 1.0, material_red));
 
-	auto material_green = make_shared<lambertian>(color(0.0, 1.0, 0.0));
-	world.add(make_shared<sphere>(point3(0,1,-1), 1.0, material_green));
+	auto material_glass = make_shared<dielectric>(1.3);
+	world.add(make_shared<sphere>(point3(0,1,-1), 1.0, material_glass));
 
 	auto material_blue = make_shared<lambertian>(color(0.0, 0.0, 1.0));
 	world.add(make_shared<sphere>(point3(0,2.70,0), 1.0, material_blue));
@@ -116,11 +116,11 @@ hittable_list scene1() {
 
 camera cam1(double aspect_ratio) {
 
-	point3 lookfrom(15,8,10);
-	point3 lookat(0,1,0);
+	point3 lookfrom(13,0.5,0);
+	point3 lookat(0,1.5,0);
 	vec3 vup(0,1,0);
 	auto vfov = 20;
-	auto dist_to_focus = 20.0;
+	auto dist_to_focus = 13.0;
 	auto aperture = 0.1;
 	
 	camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
@@ -134,22 +134,22 @@ hittable_list scene2() {
 	auto ground_material = make_shared<lambertian>(color(0.1, 0.1, 0.1));
 	world.add(make_shared<sphere>(point3(0,-10000,0), 10000, ground_material));
 
-	auto material_red = make_shared<metal>(color(1.0, 0.0, 0.0), 0.9);
+	auto material_red = make_shared<metal>(color(1.0, 0.0, 0.0), 0.05);
 	world.add(make_shared<sphere>(point3(0,1,0), 1.0, material_red));
 
-	auto material_orange = make_shared<metal>(color(1.0, 0.65, 0.0), 0.9);
+	auto material_orange = make_shared<metal>(color(1.0, 0.65, 0.0), 0.05);
 	world.add(make_shared<sphere>(point3(0,1.5,-3), 1.5, material_orange));
 
-	auto material_yellow = make_shared<metal>(color(0.85, 1.0, 0.0), 0.9);
+	auto material_yellow = make_shared<metal>(color(0.85, 1.0, 0.0), 0.05);
 	world.add(make_shared<sphere>(point3(0,2,-7), 2.0, material_yellow));
 
-	auto material_green = make_shared<metal>(color(0.0, 1.0, 0.15), 0.9);
+	auto material_green = make_shared<metal>(color(0.0, 1.0, 0.15), 0.05);
 	world.add(make_shared<sphere>(point3(0,2.5,-12), 2.5, material_green));
 
-	auto material_blue = make_shared<metal>(color(0.15, 0.0, 1.0), 0.9);
+	auto material_blue = make_shared<metal>(color(0.15, 0.0, 1.0), 0.05);
 	world.add(make_shared<sphere>(point3(0,3,-18), 3.0, material_blue));
 
-	auto material_purple = make_shared<metal>(color(0.50, 0.0, 0.50), 0.9);
+	auto material_purple = make_shared<metal>(color(0.50, 0.0, 0.50), 0.05);
 	world.add(make_shared<sphere>(point3(0,3.5,-25), 3.5, material_purple));
 
 	return world;
@@ -157,11 +157,61 @@ hittable_list scene2() {
 
 camera cam2(double aspect_ratio) {
 
-	point3 lookfrom(20,10,30);
-	point3 lookat(0,1,-15);
+	point3 lookfrom(35,10,25);
+	point3 lookat(0,1,-12);
 	vec3 vup(0,1,0);
 	auto vfov = 20;
-	auto dist_to_focus = 40.0;
+	auto dist_to_focus = 50.0;
+	auto aperture = 0.1;
+	
+	camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
+	return cam;
+}
+
+hittable_list scene3() {
+
+	hittable_list world;
+
+	// auto ground_material = make_shared<lambertian>(color(0.05, 0.05, 0.05));
+	// world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+	auto s1 = make_shared<lambertian>(color(1.0, 1.0, 1.0));
+	world.add(make_shared<sphere>(point3(0,0.5,0), 0.5, s1));
+
+	auto s2 = make_shared<lambertian>(color(0.9, 0.9, 0.9));
+	world.add(make_shared<sphere>(point3(1,0.5,-1), 0.5, s2));
+
+	auto s3 = make_shared<lambertian>(color(0.8, 0.8, 0.8));
+	world.add(make_shared<sphere>(point3(0,0.5,-2), 0.5, s3));
+
+	auto s4 = make_shared<lambertian>(color(0.7, 0.7, 0.7));
+	world.add(make_shared<sphere>(point3(-2,0.5,0), 0.5, s4));
+
+	auto s5 = make_shared<lambertian>(color(0.6, 0.6, 0.6));
+	world.add(make_shared<sphere>(point3(1,0.5,3), 0.5, s5));
+
+	auto s6 = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(6,0.5,-2), 0.5, s6));
+
+	auto s7 = make_shared<lambertian>(color(0.4, 0.4, 0.4));
+	world.add(make_shared<sphere>(point3(-2,0.5,-10), 0.5, s7));
+
+	auto s8 = make_shared<lambertian>(color(0.3, 0.3, 0.3));
+	world.add(make_shared<sphere>(point3(-15,0.5,3), 0.5, s8));
+
+	auto s9 = make_shared<lambertian>(color(0.2, 0.2, 0.2));
+	world.add(make_shared<sphere>(point3(6,0.5,24), 0.5, s9));
+
+	return world;
+}
+
+camera cam3(double aspect_ratio) {
+
+	point3 lookfrom(0,100,0);
+	point3 lookat(0,1,0);
+	vec3 vup(-1,0,0);
+	auto vfov = 20;
+	auto dist_to_focus = 100.0;
 	auto aperture = 0.1;
 	
 	camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
@@ -184,7 +234,7 @@ void render(int start, int end, std::stringstream & out, int image_width, int im
 				pixel_color += ray_color(r, world, max_depth);
 			}
 		write_color(out, pixel_color, samples_per_pixel);
-		}	
+		}
 	}
 
 	std::cerr << "Block done.\n";
@@ -197,21 +247,23 @@ int main() {
 	const auto aspect_ratio = 3.0 / 2.0;
 	const int image_width = 1200;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
-	const int samples_per_pixel = 500;
-	// const int samples_per_pixel = 10;
+	// const int samples_per_pixel = 100;
+	const int samples_per_pixel = 10;
 	const int max_depth = 10;
 
 	// World
 
 	// auto world = random_scene();
 	// auto world = scene1();
-	auto world = scene2();
+	// auto world = scene2();
+	auto world = scene3();
 
 	// Camera
 
 	// camera cam = default_cam(aspect_ratio);
 	// camera cam = cam1(aspect_ratio);
-	camera cam = cam2(aspect_ratio);
+	// camera cam = cam2(aspect_ratio);
+	camera cam = cam3(aspect_ratio);
 
 	// Render
 
@@ -227,12 +279,32 @@ int main() {
 	int s = image_height - 1;
 	for (int i = 0; i < num_threads - 1; ++i, s -= block_size + 1) {
 		streams[i] = std::stringstream();
-		threads[i] = std::thread(render, s, s - block_size, std::ref(streams[i]), image_width, image_height,
-								 samples_per_pixel, max_depth, std::ref(cam), std::ref(world));
+		threads[i] = std::thread(
+			render,
+			s,
+			s - block_size,
+			std::ref(streams[i]),
+			image_width,
+			image_height,
+			samples_per_pixel,
+			max_depth,
+			std::ref(cam),
+			std::ref(world)
+		);
 	}
 
-	threads[num_threads - 1] = std::thread(render, s, 0, std::ref(streams[num_threads - 1]), image_width,
-										   image_height, samples_per_pixel, max_depth, std::ref(cam), std::ref(world));
+	threads[num_threads - 1] = std::thread(
+		render,
+		s,
+		0,
+		std::ref(streams[num_threads - 1]),
+		image_width,
+		image_height,
+		samples_per_pixel,
+		max_depth,
+		std::ref(cam),
+		std::ref(world)
+	);
 
 	for (std::thread & t : threads)
 		t.join();
