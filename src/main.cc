897f090d6e098fdca39ecb754f6e1fe158577428
AshenSilverwolf <gilbert.gavin@outlook.com>
2022-04-20 18:41:26 -0400

    scene3 snapshot

--- src/main.cc
+++ src/main.cc
@@ -172,46 +172,79 @@ hittable_list scene3() {
 
 	hittable_list world;
 
-	// auto ground_material = make_shared<lambertian>(color(0.05, 0.05, 0.05));
-	// world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+	auto ground_material = make_shared<lambertian>(color(0.63, 0.32, 0.18));
+	world.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
 
 	auto s1 = make_shared<lambertian>(color(1.0, 1.0, 1.0));
-	world.add(make_shared<sphere>(point3(0,0.5,0), 0.5, s1));
+	world.add(make_shared<sphere>(point3(0,0.2,0), 0.2, s1));
 
-	auto s2 = make_shared<lambertian>(color(0.9, 0.9, 0.9));
-	world.add(make_shared<sphere>(point3(1,0.5,-1), 0.5, s2));
+	auto s2 = make_shared<lambertian>(color(0.95, 0.95, 0.95));
+	world.add(make_shared<sphere>(point3(-0.5,0.2,-0.5), 0.2, s2));
 
-	auto s3 = make_shared<lambertian>(color(0.8, 0.8, 0.8));
-	world.add(make_shared<sphere>(point3(0,0.5,-2), 0.5, s3));
+	auto s3 = make_shared<lambertian>(color(0.9, 0.9, 0.9));
+	world.add(make_shared<sphere>(point3(-1.1,0.2,0), 0.2, s3));
 
-	auto s4 = make_shared<lambertian>(color(0.7, 0.7, 0.7));
-	world.add(make_shared<sphere>(point3(-2,0.5,0), 0.5, s4));
+	auto s4 = make_shared<lambertian>(color(0.85, 0.85, 0.85));
+	world.add(make_shared<sphere>(point3(-1.3,0.2,0.8), 0.2, s4));
 
-	auto s5 = make_shared<lambertian>(color(0.6, 0.6, 0.6));
-	world.add(make_shared<sphere>(point3(1,0.5,3), 0.5, s5));
+	auto s5 = make_shared<lambertian>(color(0.8, 0.8, 0.8));
+	world.add(make_shared<sphere>(point3(-1,0.2,1.6), 0.2, s5));
 
-	auto s6 = make_shared<lambertian>(color(0.5, 0.5, 0.5));
-	world.add(make_shared<sphere>(point3(6,0.5,-2), 0.5, s6));
+	auto s6 = make_shared<lambertian>(color(0.75, 0.75, 0.75));
+	world.add(make_shared<sphere>(point3(-0.3,0.2,2.1), 0.2, s6));
 
-	auto s7 = make_shared<lambertian>(color(0.4, 0.4, 0.4));
-	world.add(make_shared<sphere>(point3(-2,0.5,-10), 0.5, s7));
+	auto s7 = make_shared<lambertian>(color(0.7, 0.7, 0.7));
+	world.add(make_shared<sphere>(point3(0.4,0.2,2.3), 0.2, s7));
 
-	auto s8 = make_shared<lambertian>(color(0.3, 0.3, 0.3));
-	world.add(make_shared<sphere>(point3(-15,0.5,3), 0.5, s8));
+	auto s8 = make_shared<lambertian>(color(0.65, 0.65, 0.65));
+	world.add(make_shared<sphere>(point3(1.05,0.2,2.2), 0.2, s8));
 
-	auto s9 = make_shared<lambertian>(color(0.2, 0.2, 0.2));
-	world.add(make_shared<sphere>(point3(6,0.5,24), 0.5, s9));
+	auto s9 = make_shared<lambertian>(color(0.6, 0.6, 0.6));
+	world.add(make_shared<sphere>(point3(1.8,0.2,2), 0.2, s9));
+
+	auto s10 = make_shared<lambertian>(color(0.55, 0.55, 0.55));
+	world.add(make_shared<sphere>(point3(2.5, 0.2, 1.5), 0.2, s10));
+
+	auto s11 = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+	world.add(make_shared<sphere>(point3(3,0.2,0.8), 0.2, s11));
+
+	auto s12 = make_shared<lambertian>(color(0.45, 0.45, 0.45));
+	world.add(make_shared<sphere>(point3(3.3,0.2,0), 0.2, s12));
+
+	auto s13 = make_shared<lambertian>(color(0.4, 0.4, 0.4));
+	world.add(make_shared<sphere>(point3(3.4,0.2,-0.8), 0.2, s13));
+
+	auto s14 = make_shared<lambertian>(color(0.35, 0.35, 0.35));
+	world.add(make_shared<sphere>(point3(3.3,0.2,-1.5), 0.2, s14));
+
+	auto s15 = make_shared<lambertian>(color(0.3, 0.3, 0.3));
+	world.add(make_shared<sphere>(point3(3,0.2,-2.2), 0.2, s15));
+
+	auto s16 = make_shared<lambertian>(color(0.25, 0.25, 0.25));
+	world.add(make_shared<sphere>(point3(2.6,0.2,-2.9), 0.2, s16));
+
+	auto s17 = make_shared<lambertian>(color(0.2, 0.2, 0.2));
+	world.add(make_shared<sphere>(point3(2,0.2,-3.5), 0.2, s17));
+
+	auto s18 = make_shared<lambertian>(color(0.15, 0.15, 0.15));
+	world.add(make_shared<sphere>(point3(1.4,0.2,-3.9), 0.2, s18));
+
+	auto s19 = make_shared<lambertian>(color(0.1, 0.1, 0.1));
+	world.add(make_shared<sphere>(point3(0.7,0.2,-4.2), 0.2, s19));
+
+	auto s20 = make_shared<lambertian>(color(0.05, 0.05, 0.05));
+	world.add(make_shared<sphere>(point3(0,0.2,-4.4), 0.2, s20));
 
 	return world;
 }
 
 camera cam3(double aspect_ratio) {
 
-	point3 lookfrom(0,100,0);
-	point3 lookat(0,1,0);
+	point3 lookfrom(9.54,13.68,6.84);
+	point3 lookat(1,0,-1);
 	vec3 vup(-1,0,0);
 	auto vfov = 20;
-	auto dist_to_focus = 100.0;
+	auto dist_to_focus = 15.0;
 	auto aperture = 0.1;
 	
 	camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
