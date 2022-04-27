080097c944ae09fc3314a48e50c2bc9f1ba065f4
Christopher Gray <gray.chris@protonmail.com>
2022-04-27 17:05:13 -0400

    Uses thread pools for rendering

--- src/main.cc
+++ src/main.cc
@@ -6,10 +6,12 @@
 #include "material.h"
 #include "sphere.h"
 #include "scenes.h"
+#include "thread_pool.h"
 
 #include <chrono>
 #include <functional>
 #include <iostream>
+#include <memory>
 #include <sstream>
 #include <thread>
 
@@ -97,7 +99,6 @@ camera default_cam(double aspect_ratio) {
 void render(int start, int end, std::stringstream & out, int image_width, int image_height,
 			int samples_per_pixel, int max_depth, camera & cam, hittable_list & world) {
 
-
 	for (int j = start; j >= end; --j) {
 		for (int i = 0; i < image_width; ++i) {
 			color pixel_color(0, 0, 0);
@@ -114,6 +115,21 @@ void render(int start, int end, std::stringstream & out, int image_width, int im
 	std::cerr << "Block done.\n";
 }
 
+void renderScanline(int line, std::shared_ptr<std::stringstream> & out, int image_width,
+					int image_height, int samples_per_pixel, int max_depth, camera & cam,
+					hittable_list & world) {
+
+	for (int i = 0; i < image_width; ++i) {
+		color pixel_color(0, 0, 0);
+		for (int s = 0; s < samples_per_pixel; ++s) {
+			auto u = (i + random_double()) / (image_width-1);
+			auto v = (line + random_double()) / (image_height-1);
+			ray r = cam.get_ray(u, v);
+			pixel_color += ray_color(r, world, max_depth);
+		}
+	write_color(*out, pixel_color, samples_per_pixel);
+	}
+}
 int main() {
 	
 	// * IMAGE
@@ -143,48 +159,31 @@ int main() {
 	std::cerr << "Rendering with " << num_threads << " threads.\n";
 
 	auto start = std::chrono::high_resolution_clock::now();
-	std::thread threads[num_threads];
-	std::stringstream streams[num_threads];
-	int block_size = image_height/num_threads;
+	thread_pool p(num_threads);
+	std::vector<std::shared_ptr<std::stringstream>> data;
 
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	int s = image_height - 1;
-	for (int i = 0; i < num_threads - 1; ++i, s -= block_size + 1) {
-		streams[i] = std::stringstream();
-		threads[i] = std::thread(
-			render,
-			s,
-			s - block_size,
-			std::ref(streams[i]),
-			image_width,
-			image_height,
-			samples_per_pixel,
-			max_depth,
-			std::ref(cam),
-			std::ref(world)
-		);
+	for (int i = image_height - 1; i >= 0; --i) {
+		auto scanline = std::make_shared<std::stringstream>();
+		data.push_back(scanline);
+		p.add(std::bind(
+				renderScanline,
+				i,
+				scanline,
+				image_width,
+				image_height,
+				samples_per_pixel,
+				max_depth,
+				std::ref(cam),
+				std::ref(world)
+		));
 	}
 
-	threads[num_threads - 1] = std::thread(
-		render,
-		s,
-		0,
-		std::ref(streams[num_threads - 1]),
-		image_width,
-		image_height,
-		samples_per_pixel,
-		max_depth,
-		std::ref(cam),
-		std::ref(world)
-	);
+	p.endWhenEmpty();	
 
-	for (std::thread & t : threads)
-		t.join();
-
-	for (int i = 0; i < num_threads; ++i)
-		std::cout << streams[i].str();
-
+	for (int i = 0; i < image_height; ++i)
+		std::cout << data[i]->str();
 
 	auto end = std::chrono::high_resolution_clock::now();
 
